05aa5301630b70521dc9b74d245d69cc666148c1
Agapitov_Valentin <valentin@Kartavost-PC>
2015-08-20 19:12:58 +0300

    Vulnerable web application

--- constants.php
+++ constants.php
@@ -1,7 +1,7 @@
 <?php
 	define("DB_SERVER", "localhost");
 	define("DB_USER", "root");
-	define("DB_PASS", "valentin-god");
+	define("DB_PASS", "");
 	define("DB_NAME", "test");
 
 	$con = mysql_connect(DB_SERVER,DB_USER, DB_PASS) or die(mysql_error());
